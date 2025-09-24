a413d1e3dda3c35a31128722f8df882623bed7cd
Zhanat Sarbasov <zhan.sarbasov@yandex.ru>
2025-09-24 11:44:54 +0300

    Наращивание тестовой базы и рефакторинг

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,6 +1,7 @@
 import pytest  # Импортируем pytest
-from playwright.sync_api import Playwright, \
-    Page  # Имопртируем класс страницы, будем использовать его для аннотации типов
+from playwright.sync_api import Playwright, Page  # Имопртируем класс страницы, будем использовать его для аннотации типов
+
+from pages.authentication.registration_page import RegistrationPage
 
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
@@ -20,19 +21,11 @@ def initialize_browser_state(playwright: Playwright):
     context = browser.new_context()
     page = context.new_page()
 
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill('user.name@gmail.com')
-
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill('username')
-
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill('password')
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    # Работаем с регистрационной страницей через Page Object
+    registration_page = RegistrationPage(page=page)
+    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.click_registration_button()
 
     context.storage_state(path="browser-state.json")
     browser.close()
