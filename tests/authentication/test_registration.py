6a7132157974c0cb55ddccad486aab948db96aad
Zhanat Sarbasov <zhan.sarbasov@yandex.ru>
2025-09-25 16:27:17 +0300

    Интеграция Allure в автотесты

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,12 +1,24 @@
+import allure
 import pytest
 
 from pages.authentication.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
+from allure_commons.types import Severity
 
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.tag(AllureTag.REGRESSION, AllureTag.REGISTRATION)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.AUTHENTICATION) # Добавили feature
+@allure.story(AllureStory.REGISTRATION) # Добавили story
 class TestRegistration:
+    @allure.title("Registration with correct email, username and password")
+    @allure.severity(Severity.CRITICAL)  # Добавили severity
     def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):  # Теперь используем фикстуру
         registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
         registration_page.registration_form.fill(
--- tests/dashboard/test_dashboard.py
+++ tests/dashboard/test_dashboard.py
@@ -1,11 +1,23 @@
+import allure
 import pytest
 
 from pages.dashboard.dashboard_page import DashboardPage
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
+from allure_commons.types import Severity
 
 
 @pytest.mark.dashboard
 @pytest.mark.regression
+@allure.tag(AllureTag.REGRESSION, AllureTag.DASHBOARD)
+@allure.epic(AllureEpic.LMS) # Добавили epic
+@allure.feature(AllureFeature.DASHBOARD) # Добавили feature
+@allure.story(AllureStory.DASHBOARD) # Добавили story
 class TestDashboard:
+    @allure.title("Check displaying of dashboard page")
+    @allure.severity(Severity.NORMAL)  # Добавили severity
     def test_dashboard_displaying(self, dashboard_page_with_state: DashboardPage):
         dashboard_page_with_state.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
         # Добавили проверку Navbar компонента на странице Dashboard
