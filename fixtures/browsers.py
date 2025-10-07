855456d3aa58f38a51334d39bcf04d0ec0e3a411
Zhanat Sarbasov <zhan.sarbasov@yandex.ru>
2025-10-07 18:08:31 +0300

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -5,6 +5,9 @@ from playwright.sync_api import Playwright, Page  # Импортируем кл�
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
 
+from config import settings
+from tools.routes import AppRoute
+
 
 @pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
 def chromium_page(request: SubRequest, playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
@@ -12,17 +15,21 @@ def chromium_page(request: SubRequest, playwright: Playwright) -> Page:  # Ан�
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
+    browser = playwright.chromium.launch(headless=settings.headless)
+    context = browser.new_context(base_url=settings.get_base_url())
     page = context.new_page()
 
     # Работаем с регистрационной страницей через Page Object
     registration_page = RegistrationPage(page=page)
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.visit(AppRoute.REGISTRATION)
+    registration_page.registration_form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password
+    )
     registration_page.click_registration_button()
 
-    context.storage_state(path="browser-state.json")
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 @pytest.fixture
@@ -30,5 +37,5 @@ def chromium_page_with_state(initialize_browser_state, request: SubRequest, play
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state="browser-state.json"
+        storage_state=settings.browser_state_file
     )
